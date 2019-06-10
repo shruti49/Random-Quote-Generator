d5b7c2fef1ae597912c74af433e6a4785c23d7c4
Shruti <shrutishastri39@gmail.com>
2019-06-10 16:19:17 +0530

    ignored node modules folder

---
+++ build/precache-manifest.54ca3c5b7a0aba1f8635ff3ecb7d508f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99343f249911579d04f13ee2fab9eb34",
+    "url": "/Random-Quote-Generator/index.html"
+  },
+  {
+    "revision": "4263728e775d66f31b43",
+    "url": "/Random-Quote-Generator/static/css/main.fb7f095d.chunk.css"
+  },
+  {
+    "revision": "ef17838fff8dafd427dd",
+    "url": "/Random-Quote-Generator/static/js/2.8ac72928.chunk.js"
+  },
+  {
+    "revision": "4263728e775d66f31b43",
+    "url": "/Random-Quote-Generator/static/js/main.88accb89.chunk.js"
+  },
+  {
+    "revision": "6f23a534c95c8651034b",
+    "url": "/Random-Quote-Generator/static/js/runtime~main.618461f0.js"
+  }
+]);
