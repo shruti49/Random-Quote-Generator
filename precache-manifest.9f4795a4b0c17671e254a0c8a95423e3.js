877e877ee0dc9b6af7175adb04ea9b2043406346
Shruti <shrutishastri49@gmail.com>
2019-08-11 11:14:20 +0530

    Updates

---
+++ precache-manifest.9f4795a4b0c17671e254a0c8a95423e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a7aa5f2a01889afcaf369ccd72a93a",
+    "url": "/Random-Quote-Generator/index.html"
+  },
+  {
+    "revision": "c674980b8c273ecfc596",
+    "url": "/Random-Quote-Generator/static/css/main.fbe2cc97.chunk.css"
+  },
+  {
+    "revision": "40f971173f75a5ff0132",
+    "url": "/Random-Quote-Generator/static/js/2.13cbd2b7.chunk.js"
+  },
+  {
+    "revision": "c674980b8c273ecfc596",
+    "url": "/Random-Quote-Generator/static/js/main.ab866fa8.chunk.js"
+  },
+  {
+    "revision": "6f23a534c95c8651034b",
+    "url": "/Random-Quote-Generator/static/js/runtime~main.618461f0.js"
+  }
+]);
